030bf93951cbb9feccaa113308f935f61ffca2e4
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-09 14:21:11 +0600

    first element create

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const elements = (
+//   <h1 className="heading">
+//     <span className="text">Hellow React</span>
+//     <img src=""/>  
+//     </h1>
+// );
+
+// console.log(elements);
+
+const elements = (
+  <h1 className="heading">
+    <span className="text">Hellow React</span>
+    <img src=""/>  
+    </h1>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(elements,document.getElementById('root'))
